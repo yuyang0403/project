7a06ef9c7f1aef40201097e7e598db32d951cf07
yuyang <yuyang_bj@126.com>
2018-04-05 15:38:41 +0800

    delete

--- SSHAnn/src/com/libo/dao/EmpDao.java
+++
@@ -1,9 +0,0 @@
-package com.libo.dao;
-
-import java.util.List;
-
-import com.libo.po.Emp;
-
-public interface EmpDao{
-	public List<Emp> query();
-}
--- SSHAnn/src/com/libo/service/EmpService.java
+++
@@ -1,9 +0,0 @@
-package com.libo.service;
-
-import java.util.List;
-
-import com.libo.po.Emp;
-
-public interface EmpService {
-	public List<Emp> query();
-}
